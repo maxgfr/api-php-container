21c0f4594230d4d59cd3d6604871a86e367ef524
Maxime Golfier <maxgolfier@gmail.com>
2018-07-10 11:32:18 +0200

    maxime_commit

--- index.php
+++ index.php
@@ -7,37 +7,38 @@ require_once 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-$client = new Client(['base_uri' => 'http://ec2-34-247-38-3.eu-west-1.compute.amazonaws.com:3000/api/']);
+function addOwner($client, $id) {
 
-$headers = [
-    'Content-type' => 'application/json; charset=utf-8',
-    'Accept' => 'application/json',
-];
-
-$res = $client->post('/org.magma.max.Owner', $headers , [
-    'json' =>
+    $res = $client->post('org.magma.max.Owner' ,
     [
-        '$class' => 'org.magma.max.Owner',
-        'id' => 'owner@gmail.com',
-        'address' => [
-            '$class' => 'org.magma.max.Address',
-            'city' => 'Paris',
-            'country' => 'France',
-            'street' => 'Street',
-            'zip' => '75001',
-            'id' => '1',
+        'json' =>
+        [
+            '$class' => 'org.magma.max.Owner',
+            'id' => $id,
+            'address' => [
+                '$class' => 'org.magma.max.Address',
+                'city' => 'Paris',
+                'country' => 'France',
+                'street' => 'Street',
+                'zip' => '75001'
+            ]
         ]
-    ]
-]);
+    ]);
 
-if ($res->getStatusCode() < 200 || $res->getStatusCode() >= 300) {
-    print_r('Error');
-    print_r($res->getStatusCode());
-} else {
-    $response_body = $res->getBody();
-    $response_body = json_decode($response_body, true);
-    print_r($response_body);
+    if ($res->getStatusCode() < 200 || $res->getStatusCode() >= 300) {
+        echo 'Error\n';
+        echo $res->getStatusCode();
+    } else {
+        echo 'Success\n';
+        $response_body = $res->getBody();
+        $response_body = json_decode($response_body, true);
+        print_r($response_body);
+    }
 }
+
+$client = new Client(['base_uri' => 'http://ec2-34-247-38-3.eu-west-1.compute.amazonaws.com:3000/api/']);
+addOwner($client, 'owner10@gmail.com');
+
 
 
 ?>
