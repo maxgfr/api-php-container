839bb094c314fb9bb383c0879f314c83920fb994
Maxime Golfier <maxgolfier@gmail.com>
2018-07-11 11:56:37 +0200

    maxime_commit

--- index.php
+++ index.php
@@ -7,25 +7,7 @@ require_once 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-function addParticipant($client, $sub_url, $json) {
-
-    $res = $client->post($sub_url,
-    [
-        'json' => $json
-    ]);
-
-    if ($res->getStatusCode() < 200 || $res->getStatusCode() >= 300) {
-        echo 'Error ! ';
-        echo $res->getStatusCode();
-    } else {
-        echo 'Success ! ';
-        $response_body = $res->getBody();
-        $response_body = json_decode($response_body, true);
-        print_r($response_body);
-    }
-}
-
-function getParticipant($client, $sub_url, $json) {
+function getRequest($client, $sub_url, $json) {
 
     $res = $client->get($sub_url,
     [
@@ -45,9 +27,41 @@ function getParticipant($client, $sub_url, $json) {
     }
 }
 
-function init () {
+function postRequest($client, $sub_url, $json) {
+
+    $res = $client->post($sub_url,
+    [
+        'json' => $json
+    ]);
+
+    if ($res->getStatusCode() < 200 || $res->getStatusCode() >= 300) {
+        echo 'Error ! ';
+        echo $res->getStatusCode();
+    } else {
+        echo 'Success ! ';
+        $response_body = $res->getBody();
+        $response_body = json_decode($response_body, true);
+        print_r($response_body);
+    }
+}
+
+function makePositionReading($client, $longitude, $latitude, $date, $id_container) {
+    /* Add position */
+    $owner = [
+        '$class' => 'org.magma.max.PositionReading',
+        'longitude' =>  $longitude,
+        'latitude' =>  $latitude,
+        'date' =>  $date,
+        'container' => 'resource:org.magma.max.Container#'.$id_container,
+    ];
+    $sub_url_owner='org.magma.max.PositionReading' ;
+    postRequest($client, $sub_url_owner, $owner);
+}
+
+function test() {
     $client = new Client(['base_uri' => 'http://ec2-34-247-38-3.eu-west-1.compute.amazonaws.com:3000/api/']);
 
+    /* Create a owner */
     $owner = [
         '$class' => 'org.magma.max.Owner',
         'id' => 'owner15@gmail.com',
@@ -60,8 +74,9 @@ function init () {
         ]
     ];
     $sub_url_owner='org.magma.max.Owner' ;
-    addParticipant($client, $sub_url_owner, $owner);
+    postRequest($client, $sub_url_owner, $owner);
 
+    /* Create a responsible */
     $responsible = [
         '$class' => 'org.magma.max.Responsible',
         'id' => 'responsible15@gmail.com',
@@ -73,42 +88,39 @@ function init () {
         ]
     ];
     $sub_url_resp='org.magma.max.Responsible' ;
-    addParticipant($client, $sub_url_resp, $responsible);
-}
+    postRequest($client, $sub_url_resp, $responsible);
 
-$client = new Client(['base_uri' => 'http://ec2-34-247-38-3.eu-west-1.compute.amazonaws.com:3000/api/']);
-
-$responsibleuh = [
-    'id' => 'responsible15@gmail.com',
-];
-$sub_url_responsibleuh='org.magma.max.Responsible' ;
-$resp = getParticipant($client, $sub_url_responsibleuh, $responsibleuh);
-$site = [
-    '$class' => 'org.magma.max.Site',
-    'identifier' => 'site15@gmail.com',
-    'longitude' =>  3.0862800,
-    'latitude' =>  45.7796600,
-    'geofence' =>  100,
-    'type' => 'org.magma.max.Type.FACTORY',
-    'address' => [
-        '$class' => 'org.magma.max.Address',
-        'city' => 'Clermont-Ferrand',
-        'country' => 'France',
-        'street' => 'Street'
-    ],
-    'business' => [
-        '$class' => 'org.magma.max.Responsible',
-        'id' => 'responsible15@gmail.com',
+    /* Create a site */
+    $site = [
+        '$class' => 'org.magma.max.Site',
+        'identifier' => 'site15@gmail.com',
+        'longitude' =>  3.0862800,
+        'latitude' =>  45.7796600,
+        'geofence' =>  100,
+        'type' => 'FACTORY',
+        'business' => 'resource:org.magma.max.Responsible#responsible15@gmail.com',
         'address' => [
             '$class' => 'org.magma.max.Address',
-            'city' => 'London',
-            'country' => 'UK',
+            'city' => 'Clermont-Ferrand',
+            'country' => 'France',
             'street' => 'Street'
         ]
-    ]
-];
-$sub_url_site='org.magma.max.Site';
-addParticipant($client, $sub_url_site, $site);
+    ];
+    $sub_url_site='org.magma.max.Site';
+    postRequest($client, $sub_url_site, $site);
 
+    /* Create a container */
+    $container = [
+        '$class' => 'org.magma.max.Container',
+        'containerId' => 'CONT_001',
+        'status' => 'IN_SITE',
+        'site' => 'resource:org.magma.max.Site#site15@gmail.com',
+        'owner' => 'resource:org.magma.max.Owner#owner15@gmail.com',
+    ];
+    $sub_url_container='org.magma.max.Container';
+    postRequest($client, $sub_url_container, $container);
+
+    makePositionReading($client, 3.0862800, 45.7796600, 100000, 'CONT_001');
+}
 
 ?>
